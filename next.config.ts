11a658228f220dc33b7f87ae9e84665f1fc57d39
nikyuuki <nikalyaa68@gmail.com>
2026-01-20 13:18:50 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
-/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   basePath: isProd ? '/AisyahSyazlin-Portfolio' : '',
-  assetPrefix: isProd ? '/AisyahSyazlin-Portfolio/' : '',
-  experimental: {
-    turbo: false,
-  },
+  assetPrefix: isProd
+    ? 'https://aiisyaz.github.io/AisyahSyazlin-Portfolio/'
+    : '/',
+  experimental: { turbo: false },
 };
 
 module.exports = nextConfig;
