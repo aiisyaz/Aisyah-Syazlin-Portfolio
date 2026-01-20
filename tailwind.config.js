0b2f9ac326a4bbfaa29b175db854d61ec3b6fcb7
nikyuuki <nikalyaa68@gmail.com>
2026-01-20 13:09:00 +0800

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+     "./styles/**/*.css"
+
   ],
   theme: {
     extend: {},
   },
+  darkMode: "class",
   plugins: [],
-}
+};
