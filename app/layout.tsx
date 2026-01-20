79f9af944099653098c8da7fb59170454ea7dffa
nikyuuki <nikalyaa68@gmail.com>
2026-01-20 14:44:50 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
+import './globals.css';
 import { imageBasePath } from "./lib/config";
 import ClientLayout from "./ClientLayout";
 
